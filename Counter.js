e3a33b68a97303afcac0a1e04352c185a64c1d58
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-03 21:21:17 +0900

    8-1-1.1
    useState(가장 많이 쓰이는 hook) 기능을 사용해서 숫자 카운터를 구현

--- Counter.js
+++ Counter.js
@@ -1,35 +1,17 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class Counter extends Component {
-  state = {
-    number: 0,
-    fixedNumber: 0,
-  };
-  render() {
-    const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회합니다.
-    return (
-      <div>
-        <h1>{number}</h1>
-        <h2>바뀌지 않는 값: {fixedNumber}</h2>
-        <button
-          // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정합니다.
-          onClick={() => {
-            this.setState(
-              {
-                number: number + 1,
-              },
-              () => {
-                console.log("방금 setState가 호출되었습니다.");
-                console.log(this.state);
-              }
-            );
-          }}
-        >
-          +1
-        </button>
-      </div>
-    );
-  }
-}
+const Counter = () => {
+  const [value, setValue] = useState(0);
+
+  return (
+    <div>
+      <p>
+        현재 카운터 값은 <b>{value}</b>입니다.
+      </p>
+      <button onClick={() => setValue(value + 1)}>+1</button>
+      <button onClick={() => setValue(value - 1)}>-1</button>
+    </div>
+  );
+};
 
 export default Counter;
