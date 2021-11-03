daea06552845a1bba407b5c45adf72e97b073420
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-03 20:16:53 +0900

    6-4-2.2
    배열에 새 항목을 추가할 때 배열의 push 함수를 사용하지 않고 concat을 사용했는데요. push 함수는 기존 배열 자체를 변경해 주는 반면, concat은 새로운 배열을 만들어 준다는 차이점이 있습니다.
    
    리액트에서 상태를 업데이트할 때는 기존 상태를 그대로 두면서 새로운 값을 상태로 설정해야 합니다. 이를 불변성 유지라고 하는데요. 불변성 유지를 해 주어야 나중에 리액트 컴포넌트의 성능을 최적화할 수 있습니다. 이에 대한 자세한 내용은 이 책에서 추후 알아보겠습니다.
    
    onClick 함수에서 새로운 항목을 추가할 때 객체의 id 값은 nextId를 사용하도록 하고, 클릭될 때마다 값이 1씩 올라가도록 구현했습니다. 추가로 button이 클릭될 때 기존의 input 내용을 비우는 것도 구현해 주었습니다.

--- IterationSample.js
+++ IterationSample.js
@@ -1,17 +1,42 @@
 import React, { useState } from "react";
 
 const IterationSample = () => {
+  /*컴포넌트에서 useState를 사용하여 상태를 설정하자*/
+  // 1. 데이터 배열 상태를 사용
   const [names, setNames] = useState([
     { id: 1, text: "눈사람" },
-    { id: 2, text: "얼음" },
-    { id: 3, text: "눈" },
-    { id: 4, text: "바람" },
+    { id: 2, text: "눈사람2" },
+    { id: 3, text: "눈사람3" },
+    { id: 4, text: "눈사람4" },
   ]);
+  // 2. 텍스트를 입력할 수 있는 input의 상태
   const [inputText, setInputText] = useState("");
-  const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
+  // 3. 테이터 배열에서 새로운 항목을 추가할 때 사용할 고유 id를 위한 상태
+  const [nextId, setNextId] = useState(5);
 
-  const namesList = names.map((name) => <li key={name.id}>{name.text}</li>);
-  return <ul>{namesList}</ul>;
+  const onChange = (e) => setInputText(e.target.value);
+  // onClick 함수에서 배열의 내장 함수 concat을 사용하여 새로운 항목을 추가한 배열을 만들자.
+  // concat함수는 배열에 새 항목을 추가할 때 새로운 배열을 만들어 준다
+  const onClick = () => {
+    const nextNames = names.concat({ id: nextId, text: inputText });
+    //nextId 값에 1을 더해준다
+    setNextId(nextId + 1);
+    //setNames를 통해 상태 업뎃, names 값을 업데이트한다
+    setNames(nextNames);
+    //inputText를 비운다.
+    setInputText("");
+  };
+
+  // map함수를 사용할 때 key값을 index대신 name.id 값으로 지정
+  const namesList = names.map((name) => <li key={name.id}> {name.text}</li>);
+
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{namesList}</ul>
+    </>
+  );
 };
 
 export default IterationSample;
