591c1ae7a4242103934b501ba18f2faa2cb74a47
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-03 15:24:07 +0900

    3-3-1-2
    클래스형 컴포넌트

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  const name = "리액트";
-  return <div className="react">{name}</div>;
+class App extends Component {
+  render() {
+    const name = "react";
+    return <div className="react">{name}</div>;
+  }
 }
 
 export default App;
