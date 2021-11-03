0bbd191d952f351ae8ea3771c9e6c25b04e83d2b
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-03 16:13:33 +0900

    3-3-6-1
    컴포넌트의 필수 props를 지정하거나 props의 타입(type)을 지정할 때는 propTypes를 사용
    propTypes를 사용하려면 코드 상단에 import 구문을 사용하여 불러와야 함

--- MyComponent.js
+++ MyComponent.js
@@ -1,7 +1,7 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const MyComponent = (props) => {
-  const { name, children } = props;
+const MyComponent = ({ name, children }) => {
   return (
     <div>
       안녕하세요, 제 이름은 {name}입니다. <br />
