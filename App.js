7f0de9d9d18b9fda0960883aa16464e7a00491bc
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 04:52:09 +0900

    15-2-3.1
    Provider를 사용하면 Context의 value를 변경가능

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ColorBox from "./components/ColorBox";
+import ColorContext from "./contexts/color";
 const App = () => {
   return (
-    <div>
-      <ColorBox />
-    </div>
+    <ColorContext.Provider value={{ color: "red" }}>
+      <div>
+        <ColorBox />
+      </div>
+    </ColorContext.Provider>
   );
 };
 
