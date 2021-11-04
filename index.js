d614409d9a8e75e0d8ab36992fba23a3da3b77d1
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 08:18:06 +0900

    18-1.2
    루트 리듀서를 생성

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-/* eslint-disable no-unused-vars */
+import { combineReducers } from "redux";
+import counter from "./counter";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import rootReducer from './modules';
+const rootReducer = combineReducers({
+  counter,
+});
 
-const store = createStore(rootReducer, composeWithDevTools());
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
-);
-
-serviceWorker.unregister();
+export default rootReducer;
