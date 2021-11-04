18435342b4cc304ff3a54c16789d333bd4981344
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 06:29:56 +0900

    17-4-2
     Provider 컴포넌트를 사용하여 프로젝트에 리덕스 적용하기

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -10,6 +11,11 @@ import rootReducer from './modules';
 
 const store = createStore(rootReducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 serviceWorker.unregister();
