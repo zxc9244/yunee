dd9db114d630de5349701c67ef7aacb3a5f6e782
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 01:19:56 +0900

    10-3-2-4
    TodoInsert에 넣어 준 onInsert 함수에 현재 useState를 통해 관리하고 있는 value 값을 파라미터로 넣어서 호출

--- TodoInsert.js
+++ TodoInsert.js
@@ -6,7 +6,9 @@ import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
 const TodoInsert =
-  () => {
+  ({
+    onInsert,
+  }) => {
     const [
       value,
       setValue,
@@ -29,8 +31,35 @@ const TodoInsert =
         [],
       );
 
+    const onSubmit =
+      useCallback(
+        (
+          e,
+        ) => {
+          onInsert(
+            value,
+          );
+          setValue(
+            '',
+          ); // value 값 초기화
+
+          // submit 이벤트는 브라우저에서 새로고침을 발생시킵니다.
+          // 이를 방지하기 위해 이 함수를 호출합니다.
+          e.preventDefault();
+        },
+        [
+          onInsert,
+          value,
+        ],
+      );
+
     return (
-      <form className="TodoInsert">
+      <form
+        className="TodoInsert"
+        onSubmit={
+          onSubmit
+        }
+      >
         <input
           placeholder="할 일을 입력하세요"
           value={
