7b9fbb6a34252f44bbff29287cdb227f33d2d0ec
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 07:14:02 +0900

    17-5-1.3
    액션 생성 함수를 호출하여 디스패치하는 코드가 한 줄이기 때문에 불필요한 코드 블록을 생략함

--- CounterContainer.js
+++ CounterContainer.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Counter from '../components/Counter';
+import { increase, decrease } from '../modules/counter';
 
 const CounterContainer = ({ number, increase, decrease }) => {
   return (
@@ -10,16 +11,12 @@ const CounterContainer = ({ number, increase, decrease }) => {
   );
 };
 
-const mapStateToProps = (state) => ({
-  number: state.counter.number,
-});
-const mapDispatchToProps = (dispatch) => ({
-  // 임시 함수
-  increase: () => {
-    console.log('increase');
-  },
-  decrease: () => {
-    console.log('decrease');
-  },
-});
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  (dispatch) => ({
+    increase: () => dispatch(increase()),
+    decrease: () => dispatch(decrease()),
+  }),
+)(CounterContainer);
