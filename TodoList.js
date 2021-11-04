97c88be66e01897bf1b37481db2d8695aab89fca
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 03:19:25 +0900

    11-8-2.1
    List 컴포넌트를 사용하기 위해 rowRenderer라는 함수를 사용. 이 함수는 react-virtualized의 List 컴포넌트에서 각 TodoItem을 렌더링할 때 사용

--- TodoList.js
+++ TodoList.js
@@ -1,4 +1,9 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+
+import React, {
+  useCallback,
+} from 'react';
+import { List } from 'react-virtualized';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
@@ -8,18 +13,24 @@ const TodoList =
     onRemove,
     onToggle,
   }) => {
-    return (
-      <div className="TodoList">
-        {todos.map(
-          (
-            todo,
-          ) => (
+    const rowRenderer =
+      useCallback(
+        ({
+          index,
+          key,
+          style,
+        }) => {
+          const todo =
+            todos[
+              index
+            ];
+          return (
             <TodoListItem
               todo={
                 todo
               }
               key={
-                todo.id
+                key
               }
               onRemove={
                 onRemove
@@ -27,10 +38,44 @@ const TodoList =
               onToggle={
                 onToggle
               }
+              style={
+                style
+              }
             />
-          ),
-        )}
-      </div>
+          );
+        },
+        [
+          onRemove,
+          onToggle,
+          todos,
+        ],
+      );
+    return (
+      <List
+        className="TodoList"
+        width={
+          512
+        } // 전체 크기
+        height={
+          513
+        } // 전체 높이
+        rowCount={
+          todos.length
+        } // 항목 개수
+        rowHeight={
+          57
+        } // 항목 높이
+        rowRenderer={
+          rowRenderer
+        } // 항목을 렌더링할 때 쓰는 함수
+        list={
+          todos
+        } // 배열
+        style={{
+          outline:
+            'none',
+        }} // List에 기본 적용되는 outline 스타일 제거
+      />
     );
   };
 
