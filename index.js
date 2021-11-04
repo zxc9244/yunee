27db4e267ffc3f29d9e4ff15681532d5c11993b8
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 06:29:03 +0900

    17-4-1
    스토어 생성

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-import { combineReducers } from 'redux';
-import counter from './counter';
-import todos from './todos';
+/* eslint-disable no-unused-vars */
 
-const rootReducer = combineReducers({
-  counter,
-  todos,
-});
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import rootReducer from './modules';
 
-export default rootReducer;
+const store = createStore(rootReducer);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.unregister();
