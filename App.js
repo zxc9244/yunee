089ded3ce38f90ea75b399bac3fad8b5d06a4218
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 01:17:44 +0900

    10-3-2-3.1
    todos 배열에 새 객체를 추가하는 onInsert 함수

--- App.js
+++ App.js
@@ -2,6 +2,8 @@
 
 import React, {
   useState,
+  useRef,
+  useCallback,
 } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
@@ -33,9 +35,43 @@ const App =
         ],
       );
 
+    // 고윳값으로 사용될 id
+    // ref를 사용하여 변수 담기
+    const nextId =
+      useRef(
+        4,
+      );
+
+    const onInsert =
+      useCallback(
+        (
+          text,
+        ) => {
+          const todo =
+            {
+              id: nextId.current,
+              text,
+              checked: false,
+            };
+          setTodos(
+            todos.concat(
+              todo,
+            ),
+          );
+          nextId.current += 1; // nextId 1씩 더하기
+        },
+        [
+          todos,
+        ],
+      );
+
     return (
       <TodoTemplate>
-        <TodoInsert />
+        <TodoInsert
+          onInsert={
+            onInsert
+          }
+        />
         <TodoList
           todos={
             todos
