adf1f2e9eb4f90265ebcc56a366392f08679f531
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 08:19:39 +0900

    18-1.3
    Provider로 리액트 프로젝트에 리덕스를 적용

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-import { combineReducers } from "redux";
-import counter from "./counter";
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import rootReducer from "./modules";
 
-const rootReducer = combineReducers({
-  counter,
-});
+const store = createStore(rootReducer);
 
-export default rootReducer;
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
+serviceWorker.unregister();
