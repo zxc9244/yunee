a53a73e26588a6bbe50d4fb48f6d53160d14d74f
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-04 22:23:29 +0900

    9-4-5
    styled-components를 사용할 때 반응형 디자인을 어떻게 하는지 한번 알아봅시다. 브라우저의 가로 크기에 따라 다른 스타일을 적용하기 위해서는 일반 CSS를 사용할 때와 똑같이 media 쿼리(query)를 사용하면 됨

--- StyledComponent.js
+++ StyledComponent.js
@@ -2,10 +2,21 @@ import React from "react";
 import styled, { css } from "styled-components";
 
 const Box = styled.div`
-  /* props 로 넣어준 값을 직접 전달해줄 수 있습니다. */
+  /* props로 넣어 준 값을 직접 전달해 줄 수 있습니다. */
   background: ${(props) => props.color || "blue"};
   padding: 1rem;
   display: flex;
+  /* 기본적으로는 가로 크기 1024px에 가운데 정렬을 하고
+    가로 크기가 작아짐에 따라 크기를 줄이고
+    768px 미만이 되면 꽉 채웁니다. */
+  width: 1024px;
+  margin: 0 auto;
+  @media (max-width: 1024px) {
+    width: 768px;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 const Button = styled.button`
