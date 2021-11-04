84506cf38ddc94f15db7aed48524e25ac9ec3759
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 02:48:15 +0900

    11-1.1
    실제로 랙(lag)을 경험할 수 있도록 많은 데이터를 렌더링

--- App.js
+++ App.js
@@ -9,6 +9,26 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+function createBulkTodos() {
+  const array =
+    [];
+  for (
+    let i = 1;
+    i <=
+    2500;
+    i++
+  ) {
+    array.push(
+      {
+        id: i,
+        text: `할 일 ${i}`,
+        checked: false,
+      },
+    );
+  }
+  return array;
+}
+
 const App =
   () => {
     const [
@@ -16,30 +36,14 @@ const App =
       setTodos,
     ] =
       useState(
-        [
-          {
-            id: 1,
-            text: '리액트의 기초 알아보기',
-            checked: true,
-          },
-          {
-            id: 2,
-            text: '컴포넌트 스타일링해 보기',
-            checked: true,
-          },
-          {
-            id: 3,
-            text: '일정 관리 앱 만들어 보기',
-            checked: false,
-          },
-        ],
+        createBulkTodos,
       );
 
     // 고윳값으로 사용될 id
     // ref를 사용하여 변수 담기
     const nextId =
       useRef(
-        4,
+        2501,
       );
 
     const onInsert =
@@ -58,12 +62,13 @@ const App =
               todo,
             ),
           );
-          nextId.current += 1; // nextId 1씩 더하기
+          nextId.current += 1; // nextId 1 씩 더하기
         },
         [
           todos,
         ],
       );
+
     const onRemove =
       useCallback(
         (
