eb51318d0869690ece1c7517600ddba59d462850
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 05:49:22 +0900

    16-2-8.1
    상태가 업데이트될 때마다 호출되며, 리액트의 render 함수와는 다르게 이미 html을 사용하여 만들어진 UI의 속성을 상태에 따라 변경

--- index.js
+++ index.js
@@ -41,3 +41,17 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const render = () => {
+  const state = store.getState(); // 현재 상태를 불러옵니다.
+  // 토글 처리
+  if (state.toggle) {
+    divToggle.classList.add("active");
+  } else {
+    divToggle.classList.remove("active");
+  }
+  // 카운터 처리
+  counter.innerText = state.counter;
+};
+
+render();
