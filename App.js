9693ee4fe44172311062948f977908b9b8921145
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 04:51:20 +0900

    15-2-2.2
    검정색 정사각형이 나옴

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
 import React from "react";
-import { Route } from "react-router-dom";
-import About from "./About";
-import Home from "./Home";
-
+import ColorBox from "./components/ColorBox";
 const App = () => {
   return (
     <div>
-      <Route path="/" component={Home} />
-      <Route path="/about" component={About} />
+      <ColorBox />
     </div>
   );
 };
