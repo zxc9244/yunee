6ddb49744b685366d9c1e4a8d29f0bd144ce1c1c
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 05:21:56 +0900

    15-4-2.1
    static contextType을 정의
    SelectColors 컴포넌트를 다음과 같이 클래스형으로 변환

--- SelectColors.js
+++ SelectColors.js
@@ -1,19 +1,8 @@
 import React, { Component } from "react";
-import ColorContext from "../contexts/color";
 
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
 class SelectColors extends Component {
-  static contextType = ColorContext;
-
-  handleSetColor = (color) => {
-    this.context.actions.setColor(color);
-  };
-
-  handleSetSubcolor = (subcolor) => {
-    this.context.actions.setSubcolor(subcolor);
-  };
-
   render() {
     return (
       <div>
@@ -27,11 +16,6 @@ class SelectColors extends Component {
                 width: "24px",
                 height: "24px",
                 cursor: "pointer",
-              }}
-              onClick={() => this.handleSetColor(color)}
-              onContextMenu={(e) => {
-                e.preventDefault();
-                this.handleSetSubcolor(color);
               }}
             />
           ))}
