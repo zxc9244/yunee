64e49c6ec0a6f8267d993fa8bbefddf69067efed
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 07:27:08 +0900

    17-5-2.2
     Todos 컴포넌트를 TodosContainer 컴포넌트로 교체

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
+/* eslint-disable no-unused-vars */
+
 import React from 'react';
 import CounterContainer from './containers/CounterContainer';
-import Todos from './components/Todos';
+import TodosContainer from './containers/TodosContainer';
 
 const App = () => {
   return (
     <div>
       <CounterContainer />
       <hr />
-      <Todos />
+      <TodosContainer />
     </div>
   );
 };
