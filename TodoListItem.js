6ac6e3be8bc83471d5ff06a68d84af0b70127297
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-04 23:39:13 +0900

    10-2-3.1
    일정 관리 항목이 보일 TodoListItem

---
+++ TodoListItem.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import {
+  MdCheckBoxOutlineBlank,
+  MdCheckBox,
+  MdRemoveCircleOutline,
+} from 'react-icons/md';
+import './TodoListItem.scss';
+
+const TodoListItem =
+  () => {
+    return (
+      <div className="TodoListItem">
+        <div className="checkbox">
+          <MdCheckBoxOutlineBlank />
+          <div className="text">
+            할
+            일
+          </div>
+        </div>
+        <div className="remove">
+          <MdRemoveCircleOutline />
+        </div>
+      </div>
+    );
+  };
+
+export default TodoListItem;
