670bd46d4fde51e222bd05a7817121980115e86a
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 08:22:23 +0900

    18-1.4

--- Counter.js
+++ Counter.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React from "react";
 
-const Counter = ({ number, onIncrease, onDecrease }) => {
+const Counter = ({ onIncrease, onDecrease, number }) => {
   return (
     <div>
       <h1>{number}</h1>
-      <div>
-        <button onClick={onIncrease}>+1</button>
-        <button onClick={onDecrease}>-1</button>
-      </div>
+      <button onClick={onIncrease}>+1</button>
+      <button onClick={onDecrease}>-1</button>
     </div>
   );
 };
