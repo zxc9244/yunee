1ba9180ec122d473d5d60d3887e0e599bccf7ed1
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-04 21:55:35 +0900

    9-4.2
    두 가지 버튼이 나타난다

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import React from "react";
-import styles from "./CSSModule.module.css";
+import React, { Component } from "react";
+import StyledComponent from "./StyledComponent";
 
-const CSSModule = () => {
-  return (
-    <div className={`${styles.wrapper} ${styles.inverted}`}>
-      안녕하세요, 저는 <span className="something">CSS Module!</span>
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <StyledComponent />
+      </div>
+    );
+  }
+}
 
-export default CSSModule;
+export default App;
