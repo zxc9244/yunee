fd6c194e31493123c4ed86005d2406739dc76d9b
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-04 22:43:55 +0900

    10-2-1.1

---
+++ TodoTemplate.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './TodoTemplate.scss';
+
+const TodoTemplate =
+  ({
+    children,
+  }) => {
+    return (
+      <div className="TodoTemplate">
+        <div className="app-title">
+          일정
+          관리
+        </div>
+        <div className="content">
+          {
+            children
+          }
+        </div>
+      </div>
+    );
+  };
+
+export default TodoTemplate;
