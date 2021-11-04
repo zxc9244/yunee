9b21e09ab6cf4d1970b761337947d477ebc8d1d3
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 08:11:39 +0900

    17-7-6
    connect 함수를 사용하여 컨테이너 컴포넌트를 만들었을 경우, 해당 컨테이너 컴포넌트의 부모 컴포넌트가 리렌더링될 때 해당 컨테이너 컴포넌트의 props가 바뀌지 않았다면 리렌더링이 자동으로 방지되어 성능이 최적화됩니다.
    
    useSelector를 사용하여 리덕스 상태를 조회했을 때는 이 최적화 작업이 자동으로 이루어지지 않으므로, 성능 최적화를 위해서는 React.memo를 컨테이너 컴포넌트에 사용해

--- CounterContainer.js
+++ CounterContainer.js
@@ -1,32 +1,27 @@
 import React, { useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { changeInput, insert, toggle, remove } from '../modules/todos';
-import Todos from '../components/Todos';
+import { useDispatch, useSelector } from 'react-redux';
+import Counter from '../components/Counter';
+import { decrease, increase } from '../modules/counter';
 
-const TodosContainer = () => {
-  const { input, todos } = useSelector(({ todos }) => ({
-    input: todos.input,
-    todos: todos.todos,
-  }));
+const CounterContainer = () => {
+  // useSelector Hook으로 상태 조회하기
+  const number = useSelector((state) => state.counter.number);
   const dispatch = useDispatch();
-  const onChangeInput = useCallback(
-    (input) => dispatch(changeInput(input)),
-    [dispatch],
-  );
-  const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
-  const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
-  const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
-
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
   return (
-    <Todos
-      input={input}
-      todos={todos}
-      onChangeInput={onChangeInput}
-      onInsert={onInsert}
-      onToggle={onToggle}
-      onRemove={onRemove}
-    />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
 
-export default TodosContainer;
+// 리덕스 스토어 안의 상태를 컴포넌트의 props로 넘겨주기 위해 설정하는 함수
+// const mapStateToProps = (state) => ({
+//   number: state.counter.number,
+// });
+
+// 액션 생성 함수를 컴포넌트의 props로 넘겨주기 위해 사용하는 함수
+// const mapDispatchToProps = (dispatch) =>
+// bindActionCreators 유틸 함수를 이용하면 각 액션 생성 함수를 호출하고 dispatch로 감싸지 않아도 됨
+// bindActionCreators({ increase, decrease }, dispatch);
+
+export default CounterContainer;
