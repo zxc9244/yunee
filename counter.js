315102340ff1938287c78c482632f1e12864a85f
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 08:17:13 +0900

    18-1.1
     counter 리덕스 모듈을 작성

--- counter.js
+++ counter.js
@@ -1,22 +1,19 @@
-/* eslint-disable no-unused-vars */
-import { createAction, handleActions } from 'redux-actions';
+import { createAction, handleActions } from "redux-actions";
 
-const INCREASE = 'counter/INCREASE';
-const DECREASE = 'counter/DECREASE';
+const INCREASE = "counter/INCREASE";
+const DECREASE = "counter/DECREAS";
 
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
-const initialState = {
-  number: 0,
-};
+const initialState = 0; // 상태는 꼭 객체일 필요가 없습니다. 숫자도 작동해요.
 
 const counter = handleActions(
   {
-    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
-    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+    [INCREASE]: (state) => state + 1,
+    [DECREASE]: (state) => state - 1,
   },
-  initialState,
+  initialState
 );
 
 export default counter;
