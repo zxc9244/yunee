04c6c20388d301be8d288cf2beced4fdbbe8aed0
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-06 00:11:20 +0900

    21-3-1.1
    서버 띄우기

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter } from "react-router-dom";
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import createSagaMiddleware from "redux-saga";
-import rootReducer, { rootSaga } from "./modules";
+const Koa = require('koa');
 
-const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(
-  rootReducer,
-  window.__PRELOADEDSTATE_, // 이 값을 초기 상태로 사용함
-  applyMiddleware(thunk, sagaMiddleware)
-);
+const app = new Koa();
 
-sagaMiddleware.run(rootSaga);
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
-);
 
-serviceWorker.unregister();
+app.use(ctx => {
+  ctx.body = 'hello world';
+});
+
+
+
+app.listen(4000, () => {
+  console.log('Listening to port 4000');
+});
