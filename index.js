00686f1407a50f058503570c4edf09a0988611bd
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-06 01:18:50 +0900

    21-5-5-2.1
    미들웨어를 불러와 적용

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+const Koa = require('koa');
 const Router = require('koa-router');
-const posts = require('./posts');
+const bodyParser = require('koa-bodyparser');
 
+const api = require('./api');
 
-const api = new Router();
-
-
-
-api.use('/posts', posts.routes());
+const app = new Koa();
+const router = new Router();
 
 
 
-// 라우터를 내보냅니다.
-module.exports = api;
+// 라우터 설정
+router.use('/api', api.routes()); // api 라우트 적용
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
+
+
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+
+
+app.listen(4000, () => {
+  console.log('Listening to port 4000');
+});
