1bfeead993d094fcea3f9d4046c3326997122032
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-06 03:45:33 +0900

    22-3-1.2

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+require('dotenv').config();
+const Koa = require('koa');
 const Router = require('koa-router');
-const postsCtrl = require('./posts.ctrl');
-const posts = new Router();
-posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.put('/:id', postsCtrl.replace);
-posts.patch('/:id', postsCtrl.update);
+const bodyParser = require('koa-bodyparser');
 
-module.exports = posts;
+// 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
+const { PORT } = process.env;
+
+const api = require('./api');
+
+const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+router.use('/api', api.routes()); // api 라우트 적용
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+// PORT가 지정되어 있지 않다면 4000을 사용
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('Listening to port %d', port);
+});
