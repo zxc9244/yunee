530f8f6b07a12dffd59871a7de10edb2e4f9e607
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-06 04:06:54 +0900

    22-4-1.3

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 import Router from 'koa-router';
-import * as postsCtrl from './posts.ctrl';
+import posts from './posts';
+const api = new Router();
 
-const posts = new Router();
-posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.put('/:id', postsCtrl.replace);
-posts.patch('/:id', postsCtrl.update);
+api.use('/posts', posts.routes());
 
-export default posts;
+// 라우터를 내보냅니다.
+export default api;
