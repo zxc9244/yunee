005b82bf75424c323ee6fb44f320567c04ccfcff
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 16:22:31 +0900

    18-3-1-5.5
    loading 리덕스 모듈에서 만든 액션 생성 함수는 앞에서 만든 createRequestThunk에서 사용

--- createRequestThunk.js
+++ createRequestThunk.js
@@ -5,24 +5,25 @@ export default function createRequestThunk(type, request) {
   const SUCCESS = `${type}_SUCCESSS`;
   const FAILURE = `${type}_FAILURE`;
   return (params) => async (dispatch) => {
-    dispatch({ type }); // 요청을 시작한 것을 알림
+    dispatch({ type }); // 시작됨
     dispatch(startLoading(type));
     try {
       const response = await request(params);
       dispatch({
         type: SUCCESS,
         payload: response.data,
-      }); // 요청 성공
+      }); // 성공
       dispatch(finishLoading(type));
     } catch (e) {
       dispatch({
         type: FAILURE,
         payload: e,
         error: true,
-      }); // 요청 실패 (에러 발생)
-      dispatch(finishLoading(type));
-      throw e; // 나중에 컴포넌트에서 에러를 조회할 수 있게 해줌
+      }); // 에러 발생
+      dispatch(startLoading(type));
+      throw e;
     }
   };
 }
-// 사용법 : createRequestThunk('GET_USERS', api.getUsers);
+
+// 사용법: createRequestThunk(‘GET_USERS‘,api.getUsers);
