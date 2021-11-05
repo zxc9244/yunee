db2af2581ccf19ec766f1d09b511b2354f12c45f
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 20:43:15 +0900

    20-3-5.5
    Provider 컴포넌트를 사용하여 프로젝트에 리덕스를 적용

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
-/* eslint-disable no-unused-vars */
-
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import rootReducer, { rootSaga } from "./modules";
-// import loggerMiddleware from './lib/loggerMiddleware';
-import { createLogger } from "redux-logger";
-import ReduxThunk from "redux-thunk";
-import createSagaMiddleware from "redux-saga";
+import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import rootReducer from "./modules";
 
-const logger = createLogger();
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(
-  rootReducer,
-  applyMiddleware(logger, ReduxThunk, sagaMiddleware)
-);
-sagaMiddleware.run(rootSaga);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
