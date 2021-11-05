130588a865c305822dec1db6a230e6e1fef56f83
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-06 06:07:41 +0900

    22-9-2.2
    Joi를 사용하여 ctx.request.body를 검증

--- posts.ctrl.js
+++ posts.ctrl.js
@@ -97,8 +97,30 @@ export const remove = async (ctx) => {
     tags: [‘수정’, ‘태그’]
   }
 */
+/*
+  PATCH /api/posts/:id
+  {
+    title: '수정',
+    body: '수정 내용',
+    tags: ['수정', '태그']
+  }
+*/
 export const update = async (ctx) => {
   const { id } = ctx.params;
+  // write 에서 사용한 schema 와 비슷한데, required() 가 없습니다.
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+
+  // 검증 후, 검증 실패시 에러처리
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400; // Bad Request
+    ctx.body = result.error;
+    return;
+  }
   try {
     const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
       new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
