b1b609b787ecff2d103b04e6531a20ba8d9dc364
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-06 00:45:32 +0900

    21-4.2
    index.js에서 기존 미들웨어를 모두 제거

--- index.js
+++ index.js
@@ -1,35 +1,7 @@
 const Koa = require('koa');
 
-
 const app = new Koa();
 
-  
-app.use((ctx, next) => {
-    console.log(ctx.url);
-    console.log(1);
-    if (ctx.query.authorized !== '1') {
-      ctx.status = 401; // Unauthorized
-      return;
-  }
-  next().then(() => {
-    console.log('END');
-  });
+app.listen(4000, () => {
+  console.log('Listening to port 4000');
 });
-
-  
-  app.use((ctx, next) => {
-    console.log(2);
-    next();
-  });
-  
-  
-  
-  app.use(ctx => {
-    ctx.body = 'hello world';
-  });
-  
-  
-  
-  app.listen(4000, () => {
-    console.log('Listening to port 4000');
-  });
