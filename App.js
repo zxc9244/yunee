0b13e5728bb1d0bf590c11123aa8195559ef10fb
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 17:17:06 +0900

    19-2-1.2
    App 컴포넌트를 클래스형 컴포넌트로 전환
    handleClick 메서드를 만들고, 그 내부에서 SplitMe 컴포넌트를 불러와 state에 넣음 
    render 함수에서는 state 안에 있는 SplitMe가 유효하다면 SplitMe 컴포넌트를 렌더링

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
 /* eslint-disable-no-unused-vars */
 
-import React from "react";
-import CounterContainer from "./containers/CounterContainer";
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-const App = () => {
-  return (
-    <div>
-      <CounterContainer />
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    SplitMe: null,
+  };
+  handleClick = async () => {
+    const loadedModule = await import("./SplitMe");
+    this.setState({
+      SplitMe: loadedModule.default,
+    });
+  };
+  render() {
+    const { SplitMe } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handleClick}>Hello React!</p>
+          {SplitMe && <SplitMe />}
+        </header>
+      </div>
+    );
+  }
+}
 
 export default App;
