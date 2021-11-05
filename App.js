b9f52a16de3483ec71d434536105a525999d7988
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 16:41:47 +0900

    18-3-2-2.4
    스토어에 미들웨어를 적용했다면 CounterContainer 컴포넌트를 App 컴포넌트에 렌더링

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+/* eslint-disable-no-unused-vars */
+
 import React from "react";
-import SampleContainer from "./containers/SampleContainer";
+import CounterContainer from "./containers/CounterContainer";
 
 const App = () => {
   return (
     <div>
-      <SampleContainer />
+      <CounterContainer />
     </div>
   );
 };
