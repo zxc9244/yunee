115ce3f680df36b2f4de8235a1c047f8130ce3c7
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-06 00:34:15 +0900

    21-3-2.2
    조건부로 다음 미들웨어 처리를 무시하게 만들기

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const Koa = require('koa');
 
 const app = new Koa();
 
+  
 app.use((ctx, next) => {
     console.log(ctx.url);
     console.log(1);
+    if (ctx.query.authorized !== '1') {
+      ctx.status = 401; // Unauthorized
+      return;
+    }
     next();
   });
+  
   
   
   
@@ -18,13 +24,12 @@ app.use((ctx, next) => {
   
   
   
-
-app.use(ctx => {
-  ctx.body = 'hello world';
-});
-
-
-
-app.listen(4000, () => {
-  console.log('Listening to port 4000');
-});
+  app.use(ctx => {
+    ctx.body = 'hello world';
+  });
+  
+  
+  
+  app.listen(4000, () => {
+    console.log('Listening to port 4000');
+  });
