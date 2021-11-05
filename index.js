8c19b34b212c2c897b2ae2946669358eb9e1a1c2
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 22:47:55 +0900

    20-4-6.2
    모듈을  리덕스 스토어에 redux-saga를 적용

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter } from "react-router-dom";
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import rootReducer from "./modules";
+import { combineReducers } from "redux";
+import users, { usersSaga } from "./users";
+import { all } from "redux-saga/effects";
 
-const store = createStore(
-  rootReducer,
-  window.__PRELOADED_STATE__, // 이 값을 초기상태로 사용함
-  applyMiddleware(thunk)
-);
+export function* rootSaga() {
+  yield all([usersSaga()]);
+}
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
-);
-
-serviceWorker.unregister();
+const rootReducer = combineReducers({ users });
+export default rootReducer;
