33d01d7911f1941d847045986a98e6c7f04555cc
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-06 01:01:12 +0900

    21-5-2.1
    파라미터와 쿼리를 사용하는 라우트

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
 
 const app = new Koa();
+const router = new Router();
+
+
+
+// 라우터 설정
+router.get('/', ctx => {
+  ctx.body = '홈';
+});
+router.get('/about', ctx => {
+  ctx.body = '소개';
+});
+
+
+router.get('/about/:name?', ctx => {
+    const { name } = ctx.params;
+    // name의 존재 유무에 따라 다른 결과 출력
+    ctx.body = name ? `${name}의 소개` : `소개`;
+  });
+  
+  
+  
+  router.get('/posts', ctx => {
+    const { id } = ctx.query;
+    // id의 존재 유무에 따라 다른 결과 출력
+    ctx.body = id ? `포스트 #${id}` : `포스트 아이디 없음`; 
+  });
+  
+  
+
+
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+
 
 app.listen(4000, () => {
   console.log('Listening to port 4000');
