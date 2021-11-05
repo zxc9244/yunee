a2c43ee8af176753bba09b220109ab34b3a37dea
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-06 01:06:07 +0900

    21-5-4.2
    api 라우터를 서버의 메인 라우터의 /api 경로로 설정

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+const Koa = require('koa');
 const Router = require('koa-router');
-const api = new Router();
+
+const api = require('./api');
+
+const app = new Koa();
+const router = new Router();
 
 
-api.get('/test', ctx => {
-  ctx.body = 'test 성공';
+
+// 라우터 설정
+router.use('/api', api.routes()); // api 라우트 적용
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+
+
+app.listen(4000, () => {
+  console.log('Listening to port 4000');
 });
-
-
-
-// 라우터를 내보냅니다.
-module.exports = api;
