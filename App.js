83fa8eaa0143b4027d2750e4fc0552c9a111432f
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 20:55:34 +0900

    20-4-2.5
    컨테이너 컴포넌트를 모두 작성했으면 이 컴포넌트를 보여 줄 페이지 컴포넌트를 만들고, 라우트 설정

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import Menu from "./components/Menu";
-import RedPage from "./pages/RedPage";
-import BluePage from "./pages/BluePage";
+import Menu from "./components/Menu"; // ./components/
+import RedPage from "./pages/RedPage"; // ./pages/
+import BluePage from "./pages/BluePage"; // ./pages/
+import UsersPage from "./pages/UsersPage";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <hr />
       <Route path="/red" component={RedPage} />
       <Route path="/blue" component={BluePage} />
+      <Route path="/users" component={UsersPage} />
     </div>
   );
 };
