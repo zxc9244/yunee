8376a444d2e88b395c5d6a0acc9bb7d89f7a4077
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-06 04:04:49 +0900

    22-4-1.1
    기존 코드 ES Module 형태로 바꾸기

--- posts.ctrl.js
+++ posts.ctrl.js
@@ -1,6 +1,5 @@
 let postId = 1; // id의 초깃값입니다.
 
-
 // posts 배열 초기 데이터
 const posts = [
   {
@@ -10,13 +9,11 @@ const posts = [
   },
 ];
 
-
-
 /* 포스트 작성
 POST /api/posts
 { title, body }
 */
-exports.write = ctx => {
+export const write = (ctx) => {
   // REST API의 Request Body는 ctx.request.body에서 조회할 수 있습니다.
   const { title, body } = ctx.request.body;
   postId += 1; // 기존 postId 값에 1을 더합니다.
@@ -25,26 +22,22 @@ exports.write = ctx => {
   ctx.body = post;
 };
 
-
-
 /* 포스트 목록 조회
 GET /api/posts
 */
-exports.list = ctx => {
+export const list = (ctx) => {
   ctx.body = posts;
 };
-
-
 
 /* 특정 포스트 조회
 GET /api/posts/:id
 */
-exports.read = ctx => {
+export const read = (ctx) => {
   const { id } = ctx.params;
   // 주어진 id 값으로 포스트를 찾습니다.
   // 파라미터로 받아 온 값은 문자열 형식이므로 파라미터를 숫자로 변환하거나
   // 비교할 p.id 값을 문자열로 변경해야 합니다.
-  const post = posts.find(p => p.id.toString() === id);
+  const post = posts.find((p) => p.id.toString() === id);
   // 포스트가 없으면 오류를 반환합니다.
   if (!post) {
     ctx.status = 404;
@@ -56,15 +49,13 @@ exports.read = ctx => {
   ctx.body = post;
 };
 
-
-
 /* 특정 포스트 제거
 DELETE /api/posts/:id
 */
-exports.remove = ctx => {
+export const remove = (ctx) => {
   const { id } = ctx.params;
   // 해당 id를 가진 post가 몇 번째인지 확인합니다.
-  const index = posts.findIndex(p => p.id.toString() === id);
+  const index = posts.findIndex((p) => p.id.toString() === id);
   // 포스트가 없으면 오류를 반환합니다.
   if (index === -1) {
     ctx.status = 404;
@@ -78,17 +69,15 @@ exports.remove = ctx => {
   ctx.status = 204; // No Content
 };
 
-
-
 /* 포스트 수정(교체)
 PUT /api/posts/:id
 { title, body }
 */
-exports.replace = ctx => {
+export const replace = (ctx) => {
   // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용합니다.
   const { id } = ctx.params;
   // 해당 id를 가진 post가 몇 번째인지 확인합니다.
-  const index = posts.findIndex(p => p.id.toString() === id);
+  const index = posts.findIndex((p) => p.id.toString() === id);
   // 포스트가 없으면 오류를 반환합니다.
   if (index === -1) {
     ctx.status = 404;
@@ -106,17 +95,15 @@ exports.replace = ctx => {
   ctx.body = posts[index];
 };
 
-
-
 /* 포스트 수정(특정 필드 변경)
 PATCH /api/posts/:id
 { title, body }
 */
-exports.update = ctx => {
+export const update = (ctx) => {
   // PATCH 메서드는 주어진 필드만 교체합니다.
   const { id } = ctx.params;
   // 해당 id를 가진 post가 몇 번째인지 확인합니다.
-  const index = posts.findIndex(p => p.id.toString() === id);
+  const index = posts.findIndex((p) => p.id.toString() === id);
   // 포스트가 없으면 오류를 반환합니다.
   if (index === -1) {
     ctx.status = 404;
