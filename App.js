22219880ed9b7fca485aae6831b4d0be464f3241
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 17:26:54 +0900

    19-2-3.1
    컴포넌트를 미리 불러오는(preload) 방법

--- App.js
+++ App.js
@@ -4,22 +4,29 @@ import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import loadable from "@loadable/component";
-const SplitMe = loadable(() => import("./SplitMe"));
+
+const SplitMe = loadable(() => import("./SplitMe"), {
+  fallback: <div>loading…</div>,
+});
 
 function App() {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
   };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React!
+        </p>
         {visible && <SplitMe />}
       </header>
     </div>
   );
 }
-
 export default App;
