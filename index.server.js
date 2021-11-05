8fe2a32dfac1e0e00e5dd08b35dd6391df398287
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-05 23:05:50 +0900

    20-4-8.1
    redux-thunk를 사용하면 Preloader를 통해 호출한 함수들이 Promise를 반환하지만, redux-saga를 사용하면 Promise를 반환하지 않기 때문에 추가 작업이 필요합니다.

--- index.server.js
+++ index.server.js
@@ -47,7 +47,7 @@ const app = express();
 
 // 서버사이드 렌더링을 처리 할 핸들러 함수입니다.
 const serverRender = async (req, res, next) => {
-  // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버사이드 렌더링을 해줍니다.
+  // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버 사이드 렌더링을 해 줍니다.
 
   const context = {};
   const sagaMiddleware = createSagaMiddleware();
@@ -56,6 +56,8 @@ const serverRender = async (req, res, next) => {
     rootReducer,
     applyMiddleware(thunk, sagaMiddleware)
   );
+
+  sagaMiddleware.run(rootSaga);
 
   const sagaPromise = sagaMiddleware.run(rootSaga).toPromise();
 
