8d56fa878406da48382fed234722ff62b23d559d
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-09 18:44:13 +0900

    23-2-2.1
    스태틱 메서드 만들기

--- user.js
+++ user.js
@@ -5,7 +5,6 @@ const UserSchema = new Schema({
   username: String,
   hashedPassword: String,
 });
-
 UserSchema.methods.setPassword = async function (password) {
   const hash = await bcrypt.hash(password, 10);
   this.hashedPassword = hash;
@@ -14,6 +13,10 @@ UserSchema.methods.setPassword = async function (password) {
 UserSchema.methods.checkPassword = async function (password) {
   const result = await bcrypt.compare(password, this.hashedPassword);
   return result; // true / false
+};
+
+UserSchema.statics.findByUsername = function (username) {
+  return this.findOne({ username });
 };
 
 const User = mongoose.model('User', UserSchema);
