103c64b4a75c610f9a0cf1b8982b9c55ed16c3bc
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-09 23:07:25 +0900

    25-3-1.3
     Editor 컴포넌트를 수정

--- Editor.js
+++ Editor.js
@@ -34,7 +34,7 @@ const QuillWrapper = styled.div`
   }
 `;
 
-const Editor = () => {
+const Editor = ({ title, body, onChangeField }) => {
   const quillElement = useRef(null); // Quill을 적용할 DivElement를 설정
   const quillInstance = useRef(null); // Quill 인스턴스를 설정
 
@@ -53,11 +53,28 @@ const Editor = () => {
         ],
       },
     });
-  }, []);
+
+    // quill에 text-change 이벤트 핸들러 등록
+    // 참고: https://quilljs.com/docs/api/#events
+    const quill = quillInstance.current;
+    quill.onChangeField('text-change', (delta, oldDelta, source) => {
+      if (source === 'user') {
+        onChangeField({ key: 'body', value: quill.root.innerHTML });
+      }
+    });
+  }, [onChangeField]);
+
+  const onChangeTitle = (e) => {
+    onChangeField({ key: 'title', value: e.current.value });
+  };
 
   return (
     <EditorBlock>
-      <TitleInput placeholder="제목을 입력하세요." />
+      <TitleInput
+        placeholder="제목을 입력하세요."
+        onChange={onChangeTitle}
+        value={title}
+      />
       <QuillWrapper>
         <div ref={quillElement} />
       </QuillWrapper>
