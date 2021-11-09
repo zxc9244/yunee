1dc9785071212f0f2e63becfcb66c8fd28eb06f5
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-09 21:36:03 +0900

    24-2-4.2
    사용자의 상태를 담을 user라는 리덕스 모듈

--- user.js
+++ user.js
@@ -1,60 +1,44 @@
-import mongoose, { Schema } from 'mongoose';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import { createAction, handleActions } from 'redux-actions';
+import { takeLatest } from 'redux-saga/effects';
+import * as authAPI from '../lib/api/auth';
+import createRequestSaga, {
+  createRequestActionTypes,
+} from '../lib/createRequestSaga';
 
-const UserSchema = new Schema({
-  username: String,
-  hashedPassword: String,
-});
-UserSchema.methods.setPassword = async function (password) {
-  const hash = await bcrypt.hash(password, 10);
-  this.hashedPassword = hash;
+const TEMP_SET_USER = 'user/TEMP_SET_USER'; // 새로고침 이후 임시 로그인 처리
+// 회원 정보 확인
+const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] =
+  createRequestActionTypes('user/CHECK');
+
+export const tempSetUser = createAction(TEMP_SET_USER, (user) => user);
+export const check = createAction(CHECK);
+
+const checkSaga = createRequestSaga(CHECK, authAPI.check);
+export function* userSaga() {
+  yield takeLatest(CHECK, checkSaga);
+}
+
+const initialState = {
+  user: null,
+  checkError: null,
 };
 
-UserSchema.methods.checkPassword = async function (password) {
-  const result = await bcrypt.compare(password, this.hashedPassword);
-  return result; // true / false
-};
-
-UserSchema.statics.findByUsername = function (username) {
-  return this.findOne({ username });
-};
-
-UserSchema.methods.serialize = function () {
-  const data = this.toJSON();
-  delete data.hashedPassword;
-  return data;
-};
-
-UserSchema.methods.generateToken = function () {
-  const token = jwt.sign(
-    // 첫 번째 파라미터에는 토큰 안에 집어넣고 싶은 데이터를 넣습니다.
-    {
-      _id: this.id,
-      username: this.username,
-    },
-    process.env.JWT_SECRET, // 두 번째 파라미터에는 JWT 암호를 넣습니다.
-    {
-      expiresIn: '7d', // 7일 동안 유효함
-    },
-  );
-  return token;
-};
-
-UserSchema.methods.generateToken = function () {
-  const token = jwt.sign(
-    // 첫 번째 파라미터에는 토큰 안에 집어넣고 싶은 데이터를 넣습니다.
-    {
-      _id: this.id,
-      username: this.username,
-    },
-    process.env.JWT_SECRET, // 두 번째 파라미터에는 JWT 암호를 넣습니다.
-    {
-      expiresIn: '3d', // 3일 동안 유효함
-    },
-  );
-  return token;
-};
-
-const User = mongoose.model('User', UserSchema);
-export default User;
+export default handleActions(
+  {
+    [TEMP_SET_USER]: (state, { payload: user }) => ({
+      ...state,
+      user,
+    }),
+    [CHECK_SUCCESS]: (state, { payload: user }) => ({
+      ...state,
+      user,
+      checkError: null,
+    }),
+    [CHECK_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      user: null,
+      checkError: error,
+    }),
+  },
+  initialState,
+);
