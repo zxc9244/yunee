95c3868b91ba75ea5cfe3ca0c0ef86fb27d2d981
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-09 20:05:01 +0900

    24-1-2.6
    프로젝트 엔트리 파일인 index.js에서 BrowserRouter로 App 컴포넌트를 감싸기

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-import Router from 'koa-router';
-import * as postsCtrl from './posts.ctrl';
-import checkLoggedIn from '../../lib/checkLoggedIn';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-const posts = new Router();
-
-posts.get('/', postsCtrl.list);
-posts.post('/', checkLoggedIn, postsCtrl.write);
-
-const post = new Router(); // /api/posts/:id
-post.get('/', postsCtrl.read);
-post.delete('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove);
-post.patch('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update);
-posts.use('/:id', postsCtrl.getPostById, post.routes());
-
-export default posts;
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
