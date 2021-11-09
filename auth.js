7d9208d65f90d214b1fa421c06181bab3491ad5c
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-09 21:19:58 +0900

    24-2-3-3.1
    회원 인증에 필요한 API를 사용하기 쉽도록 함수화하여 파일로 작성

--- auth.js
+++ auth.js
@@ -1,43 +1,12 @@
-import { createAction, handleActions } from 'redux-actions';
-import produce from 'immer';
+import client from './client';
 
-const CHANGE_FIELD = 'auth/CHANGE_FIELD';
-const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+// 로그인
+export const login = ({ username, password }) =>
+  client.post('/api/auth/login', { username, password });
 
-export const changeField = createAction(
-  CHANGE_FIELD,
-  ({ form, key, value }) => ({
-    form, // register , login
-    key, // username, password, passwordConfirm
-    value, // 실제 바꾸려는 값
-  }),
-);
-export const initializeForm = createAction(INITIALIZE_FORM, (form) => form); // register / login
+// 회원가입
+export const register = ({ username, password }) =>
+  client.post('/api/auth/register', { username, password });
 
-const initialState = {
-  register: {
-    username: '',
-    password: '',
-    passwordConfirm: '',
-  },
-  login: {
-    username: '',
-    password: '',
-  },
-};
-
-const auth = handleActions(
-  {
-    [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
-      produce(state, (draft) => {
-        draft[form][key] = value; // 예: state.register.username을 바꾼다.
-      }),
-    [INITIALIZE_FORM]: (state, { payload: form }) => ({
-      ...state,
-      [form]: initialState[form],
-    }),
-  },
-  initialState,
-);
-
-export default auth;
+// 로그인 상태 확인
+export const check = () => client.get('/api/auth/check');
