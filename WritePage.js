cdfbcd04ab252e70ec876cd6570423ffbbf98d84
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-09 22:56:47 +0900

    25-2-1.2
     WritePage에서 Editor 하단에 렌더링

--- WritePage.js
+++ WritePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Editor from '../components/write/Editor';
+import TagBox from '../components/write/TagBox';
 import Responsive from '../components/common/Responsive';
 
 const WritePage = () => {
   return (
     <Responsive>
       <Editor />
+      <TagBox />
     </Responsive>
   );
 };
