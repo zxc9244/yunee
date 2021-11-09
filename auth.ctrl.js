74e7a8605d6ada86fc85de9a6225491717976699
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-09 19:22:24 +0900

    23-4-3.4
    콘솔에 토큰 정보를 출력하는 코드는 이후 토큰이 만료되기 전에 재발급해 주는 기능을 구현

--- auth.ctrl.js
+++ auth.ctrl.js
@@ -82,3 +82,16 @@ export const login = async (ctx) => {
     ctx.throw(500, e);
   }
 };
+
+/*
+  GET /api/auth/check
+*/
+export const check = async (ctx) => {
+  const { user } = ctx.state;
+  if (!user) {
+    // 로그인 중 아님
+    ctx.status = 401; // Unauthorized
+    return;
+  }
+  ctx.body = user;
+};
