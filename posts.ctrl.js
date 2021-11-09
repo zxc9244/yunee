a04c79968807a0c8f086227761416b90ba85a8be
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-09 17:51:57 +0900

    22-10-6.1
     body의 길이가 200자 이상이면 뒤에 ‘…’을 붙이고 문자열을 자르는 기능을 구현

--- posts.ctrl.js
+++ posts.ctrl.js
@@ -54,8 +54,8 @@ export const write = async (ctx) => {
   GET /api/posts
 */
 export const list = async (ctx) => {
-  // query는 문자열이기 때문에 숫자로 변환해 주어야 합니다.
-  // 값이 주어지지 않았다면 1을 기본으로 사용합니다.
+  // query 는 문자열이기 때문에 숫자로 변환해주어야합니다.
+  // 값이 주어지지 않았다면 1 을 기본으로 사용합니다.
   const page = parseInt(ctx.query.page || '1', 10);
 
   if (page < 1) {
@@ -68,10 +68,15 @@ export const list = async (ctx) => {
       .sort({ _id: -1 })
       .limit(10)
       .skip((page - 1) * 10)
+      .lean()
       .exec();
     const postCount = await Post.countDocuments().exec();
     ctx.set('Last-Page', Math.ceil(postCount / 10));
-    ctx.body = posts;
+    ctx.body = posts.map((post) => ({
+      ...post,
+      body:
+        post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+    }));
   } catch (e) {
     ctx.throw(500, e);
   }
