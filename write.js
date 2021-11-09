055c7137ba4aed1b16daa744bf8b0d7458485a3a
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-10 00:41:55 +0900

    27-1-2.1
    write 리덕스 모듈에 SET_ORIGINAL_POST라는 액션을 만든다. 이 액션은 현재 보고있는 포스트 정보를 write모듈에서 관리하는 상태에 넣는다.

--- write.js
+++ write.js
@@ -9,6 +9,7 @@ const INITIALIZE = 'write/INITIALIZE'; // 모든 내용 초기화
 const CHANGE_FIELD = 'write/CHANGE_FIELD'; // 특정 key 값 바꾸기
 const [WRITE_POST, WRITE_POST_SUCCESS, WRITE_POST_FAILURE] =
   createRequestActionTypes('write/WRITE_POST'); // 포스트 작성
+const SET_ORIGINAL_POST = 'write/SET_ORIGINAL_POST';
 
 export const initialize = createAction(INITIALIZE);
 export const changeField = createAction(CHANGE_FIELD, ({ key, value }) => ({
@@ -21,7 +22,9 @@ export const writePost = createAction(WRITE_POST, ({ title, body, tags }) => ({
   tags,
 }));
 
-// 사가 생성
+export const setOriginalPost = createAction(SET_ORIGINAL_POST, (post) => post);
+
+// saga 생성
 const writePostSaga = createRequestSaga(WRITE_POST, postsAPI.writePost);
 export function* writeSaga() {
   yield takeLatest(WRITE_POST, writePostSaga);
@@ -33,11 +36,12 @@ const initialState = {
   tags: [],
   post: null,
   postError: null,
+  originalPostId: null,
 };
 
 const write = handleActions(
   {
-    [INITIALIZE]: (state) => initialState, // initialState를 넣으면 초기 상태로 바뀜
+    [INITIALIZE]: (state) => initialState, // initialState를 넣으면 초기상태로 바뀜
     [CHANGE_FIELD]: (state, { payload: { key, value } }) => ({
       ...state,
       [key]: value, // 특정 key 값을 업데이트
@@ -57,6 +61,13 @@ const write = handleActions(
     [WRITE_POST_FAILURE]: (state, { payload: postError }) => ({
       ...state,
       postError,
+    }),
+    [SET_ORIGINAL_POST]: (state, { payload: post }) => ({
+      ...state,
+      title: post.title,
+      body: post.body,
+      tags: post.tags,
+      originalPostId: post._id,
     }),
   },
   initialState,
