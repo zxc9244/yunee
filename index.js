f080bfabe86d2abcb2d47ccaf299a2a1df557a31
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-09 22:25:19 +0900

    24-3-2-2.2
    sagaMiddleware.run이 호출된 이후에 loadUser 함수를 호출하는 것이 중요합니다. loadUser 함수를 먼저 호출하면 CHECK 액션을 디스패치했을 때 사가에서 이를 제대로 처리하지 않습니다.

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
-import { combineReducers } from 'redux';
-import { all } from 'redux-saga/effects';
-import auth, { authSaga } from './auth';
-import loading from './loading';
-import user, { userSaga } from './user';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer, { rootSaga } from './modules';
+import { tempSetUser, check } from './modules/user';
 
-const rootReducer = combineReducers({
-  auth,
-  loading,
-  user,
-});
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
+);
 
-export function* rootSaga() {
-  yield all([authSaga(), userSaga()]);
+function loadUser() {
+  try {
+    const user = localStorage.getItem('user');
+    if (!user) return; // 로그인 상태가 아니라면 아무것도 안함
+
+    store.dispatch(tempSetUser(user));
+    store.dispatch(check());
+  } catch (e) {
+    console.log('localStorage is not working');
+  }
 }
 
-export default rootReducer;
+sagaMiddleware.run(rootSaga);
+loadUser();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'),
+);
