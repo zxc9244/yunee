bc9de231e2d231eec400d2709215a06d0213a43f
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-09 23:03:08 +0900

    25-3.2
    이제 루트 리듀서에 포함

--- index.js
+++ index.js
@@ -1,41 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import createSagaMiddleware from 'redux-saga';
-import rootReducer, { rootSaga } from './modules';
-import { tempSetUser, check } from './modules/user';
+import { combineReducers } from 'redux';
+import { all } from 'redux-saga/effects';
+import auth, { authSaga } from './auth';
+import loading from './loading';
+import user, { userSaga } from './user';
+import write from './write';
 
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(sagaMiddleware)),
-);
+const rootReducer = combineReducers({
+  auth,
+  loading,
+  user,
+  write,
+});
 
-function loadUser() {
-  try {
-    const user = localStorage.getItem('user');
-    if (!user) return; // 로그인 상태가 아니라면 아무것도 안함
-
-    store.dispatch(tempSetUser(user));
-    store.dispatch(check());
-  } catch (e) {
-    console.log('localStorage is not working');
-  }
+export function* rootSaga() {
+  yield all([authSaga(), userSaga()]);
 }
 
-sagaMiddleware.run(rootSaga);
-loadUser();
-
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root'),
-);
+export default rootReducer;
