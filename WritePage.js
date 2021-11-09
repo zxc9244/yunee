8692ec87cd1fd554b58befd79f32aff782e36124
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-09 23:08:59 +0900

    25-3-2.2
     WritePage에서 TagBox를 TagBoxContainer로 대체

--- WritePage.js
+++ WritePage.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import TagBox from '../components/write/TagBox';
 import WriteActionButtons from '../components/write/WriteActionButtons';
 import Responsive from '../components/common/Responsive';
 import EditorContainer from '../containers/write/EditorContainer';
+import TagBoxContainer from '../containers/write/TagBoxContainer';
 
 const WritePage = () => {
   return (
     <Responsive>
       <EditorContainer />
-      <TagBox />
+      <TagBoxContainer />
       <WriteActionButtons />
     </Responsive>
   );
