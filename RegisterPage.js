ce1a2135933f05a80ebf0b8c6e85cc63ad7e0dcf
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-09 21:03:22 +0900

    24-2-1-2.4
    RegisterPage에서 AuthForm을 사용할 때 type을 넣어 주기

--- RegisterPage.js
+++ RegisterPage.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import AuthTemplate from '../components/auth/AuthTemplate';
 import AuthForm from '../components/auth/AuthForm';
-
 const RegisterPage = () => {
   return (
     <AuthTemplate>
-      <AuthForm />
+      <AuthForm type="register" />
     </AuthTemplate>
   );
 };
