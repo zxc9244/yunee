c6aa54268009346f50e4c599a26264ca80bd5f4f
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-09 18:51:28 +0900

    23-2.3
    auth 라우터를 api 라우터에 적용

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import Router from 'koa-router';
-import * as authCtrl from './auth.ctrl';
-const auth = new Router();
+import posts from './posts';
+import auth from './auth';
 
-auth.post('/register', authCtrl.register);
-auth.post('/login', authCtrl.login);
-auth.get('/check', authCtrl.check);
-auth.post('/logout', authCtrl.logout);
+const api = new Router();
 
-export default auth;
+api.use('/posts', posts.routes());
+api.use('/auth', auth.routes());
+
+// 라우터를 내보냅니다.
+export default api;
