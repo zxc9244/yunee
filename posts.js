f5808de4986d19694f8041275e78acd5e069a413
zxc9244 <90129339+zxc9244@users.noreply.github.com>
2021-11-09 23:53:42 +0900

    26-2-2.2
    요청의 상태를 관리하는 리덕스 모듈

--- posts.js
+++ posts.js
@@ -1,16 +1,40 @@
-import qs from 'qs';
-import client from './client';
+import { createAction, handleActions } from 'redux-actions';
+import createRequestSaga, {
+  createRequestActionTypes,
+} from '../lib/createRequestSaga';
+import * as postsAPI from '../lib/api/posts';
+import { takeLatest } from 'redux-saga/effects';
 
-export const writePost = ({ title, body, tags }) =>
-  client.post('/api/posts', { title, body, tags });
+const [LIST_POSTS, LIST_POSTS_SUCCESS, LIST_POSTS_FAILURE] =
+  createRequestActionTypes('posts/LIST_POSTS');
 
-export const readPost = (id) => client.get(`/api/posts/${id}`);
+export const listPosts = createAction(
+  LIST_POSTS,
+  ({ tag, username, page }) => ({ tag, username, page }),
+);
 
-export const listPosts = ({ page, username, tag }) => {
-  const queryString = qs.stringify({
-    page,
-    username,
-    tag,
-  });
-  return client.get(`/api/posts?${queryString}`);
+const listPostsSaga = createRequestSaga(LIST_POSTS, postsAPI.listPosts);
+export function* postsSaga() {
+  yield takeLatest(LIST_POSTS, listPostsSaga);
+}
+
+const initialState = {
+  posts: null,
+  error: null,
 };
+
+const posts = handleActions(
+  {
+    [LIST_POSTS_SUCCESS]: (state, { payload: posts }) => ({
+      ...state,
+      posts,
+    }),
+    [LIST_POSTS_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      error,
+    }),
+  },
+  initialState,
+);
+
+export default posts;
